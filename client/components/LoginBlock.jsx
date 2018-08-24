11baad586d7dba17a46590de5e3fe4188c9fe2ac
JChan <jchan@JCs-MacBook-Pro.local>
2018-08-24 11:24:12 -0700

    initial commit

---
+++ client/components/LoginBlock.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class LoginBlock extends Component {
+  render() {
+    <form>
+      <input type="text"/>
+      <input type="text"/>
+    </form>
+  }
+}
+
+export default LoginBlock;
