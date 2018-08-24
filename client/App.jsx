2754c62fcf14123a329ce9edf53d1dbc78880637
seano <sth.xxx@gmail.com>
2018-08-24 15:35:10 -0700

    moved App to /client/

---
+++ client/App.jsx
@@ -0,0 +1,31 @@
+/**
+ * ************************************
+ *
+ * @module  App.jsx
+ * @author seano
+ * @date
+ * @description app component that renders everything
+ *
+ * ************************************
+ */
+
+import React, {Component} from 'react';
+import Main from './containers/MainContainer.jsx';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <Main />
+      </div>
+    )
+  }
+}
+
+export default App;
