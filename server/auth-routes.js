8f8524dd0dc039fc71ec0f02dc59d2f79695d351
gerretkubota <gtkubota@cpp.edu>
2018-08-29 11:01:36 -0700

    wtf

--- server/auth-routes.js
+++ server/auth-routes.js
@@ -1,42 +1,22 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-// route login
 router.get('/login', (req, res) => {
-    res.send(`i'm here`);
+    res.send('logged in?');
 });
-// route logout
-router.get('/logout', (req, res) => {
-    res.send('pz');
-})
 
-// auth with google
-// google strategy being passed down from passport-setup
-// when passport.use is used.
-// .authenticate 'google' and extract profile info to use
+router.get('/logout', (req, res) => {
+    res.send('logging out');
+});
+
+// auth google
 router.get('/google', passport.authenticate('google', {
     scope: ['profile']
 }));
 
-// successfully getting message back from google after signing in
-router.get('/google/redirect', 
-    passport.authenticate('google'), 
-    (req, res) => {
-        res.send('you reached the callback uri');
-    })
-// router.get('/google', passport.authenticate('google', {
-//     scope: ['profile']
-// }));
-
-// router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-//     // res.send(req.user);
-//     res.redirect('/profile');
-// });
-
-// router.get('/logout', (req, res) => {
-// //   req.logout();
-//   res.send('bye')
-//   // res.redirect('/');
-// });
+// callback route for google to redirect to
+router.get('/google/redirect', (req, res) => {
+    res.send('you reached the callback URI');
+})
 
 module.exports = router;
--- server/passport-setup.js
+++ server/passport-setup.js
@@ -1,32 +1,17 @@
 require('dotenv').config();
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require('passport');
-// const expressSession = require('express-session');
+const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
-// const db = require('./models/db.js');
 
-// // a way of storing certain data from the user data object to the session (store into a cookie)
-// passport.serializeUser((user, done) => {
-//   done(null, user);
-// });
 
-// // a way of using the piece of data that has been obtained from the serialization, to 
-// // identify which user to retrieve the information from
-// passport.deserializeUser((user, done) => {
-//   done(null, user);
-// });
-
-console.log('HELLO?');
 passport.use(new GoogleStrategy({
+    // options for the google strat
     callbackURL: '/auth/google/redirect',
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
-  }, function() {
-    // passport callback
-    // accessToken => from google
-    console.log('passport callback firebacked');
-    // console.log(profile);
+  }), () => {
+    // passport callback function
+    res.send('work?');
   }
-))
-
+)
